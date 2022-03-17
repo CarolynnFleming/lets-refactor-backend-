4060f8626cc0c4a7748eb3fb44a7a3229767713f
CarolynnFleming <cfcatpink@gmail.com>
2022-03-16 17:04:56 -0700

    setup sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE songs (
+    CREATE TABLE GENERATED ALWAYS AS IDENTITY,
+    product TEXT NPT NULL,
+    quantity INT NOT NULL
+);
