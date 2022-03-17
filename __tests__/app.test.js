d9cb4f76235234f4f7f458310b2259edee926bbf
CarolynnFleming <cfcatpink@gmail.com>
2022-03-16 21:02:43 -0700

    hmm

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,26 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Song = require('../lib/models/Song');
 
-describe('alchemy-app routes', () => {
+describe('refactory routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should be able to create a song', async () => {
+    const res = await request(app)
+      .post('/api/v1/songs')
+      .send({ artist: 'Beyonce', genre: 'alternative' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      artist: 'Beyonce',
+      genre: 'alternative',
+    });
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,9 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS songs;
  
     CREATE TABLE songs (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
-    product TEXT NPT NULL,
-    quantity INT NOT NULL
+    artist TEXT NOT NULL,
+    genre INT NOT NULL
 );
