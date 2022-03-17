bee124b7accef9f11321513acc40a811827fbebc
CarolynnFleming <cfcatpink@gmail.com>
2022-03-16 22:34:02 -0700

    test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,15 +13,61 @@ describe('refactory routes', () => {
     pool.end();
   });
 
-  it('should be able to create a song', async () => {
+  it('should be able to create an song', async () => {
     const res = await request(app)
       .post('/api/v1/songs')
-      .send({ artist: 'Beyonce', genre: 'alternative' });
+      .send({ artist: 'Widget', genre: 1 });
 
     expect(res.body).toEqual({
       id: expect.any(String),
-      artist: 'Beyonce',
-      genre: 'alternative',
+      product: 'Widget',
+      genre: 1,
     });
   });
+
+  it('should be able to list an song by id', async () => {
+    const song = await Song.insert({ artist: 'Widget', genre: 1 });
+    const res = await request(app).get(`/api/v1/songs/${song.id}`);
+
+    expect(res.body).toEqual(song);
+  });
+
+  it('should be able to list songs', async () => {
+    await Song.insert({ artist: 'Widget', genre: 'blues' });
+    const res = await request(app).get('/api/v1/songs');
+
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        artist: 'Widget',
+        genre: 1,
+      },
+    ]);
+  });
+
+  it('should be able to update an song', async () => {
+    const song = await Song.insert({ artist: 'Widget', genre: 'blues' });
+    const res = await request(app)
+      .patch(`/api/v1/songs/${song.id}`)
+      .send({ artist: 'Thingamajig', genre: 2 });
+
+    const expected = {
+      id: expect.any(String),
+      artist: 'Thingamajig',
+      genre: 2,
+    };
+
+    expect(res.body).toEqual(expected);
+    expect(await Song.getById(song.id)).toEqual(expected);
+  });
+
+  it('should be able to delete an order', async () => {
+    const song = await Song.insert({ artist: 'Widget', genre: 1 });
+    const res = await request(app).delete(`/api/v1/songs/${song.id}`);
+
+    expect(res.body).toEqual(song);
+    expect(await Song.getById(song.id)).toBeNull();
+  });
 });
+
+
